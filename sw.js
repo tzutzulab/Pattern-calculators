7033f39aaa0dddfeaacf2e5c5018a6b6bb3539e1
tzutzulab <ella93103@gmail.com>
2026-05-01 15:08:15 +0800

    優化安裝機制

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-const CACHE_NAME = 'pattern-calc-v1';
 const ASSETS = [
   'index.html',
   '女子原型計算機.html',
@@ -6,23 +5,7 @@ const ASSETS = [
   '兒童原型計算機.html',
   '基本長褲計算機.html',
   '基本窄裙計算機.html',
-  'icon.png'
+  'icon.png',
+  'manifest.json'
 ];
 
-// 安裝 Service Worker 並快取檔案
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// 攔截請求，優先從快取讀取
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
